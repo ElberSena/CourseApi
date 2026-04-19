bcc8173b1cf2f33b2e14f93552f9c5158a3c39a9
Elber Sena <elbersenna@hotmail.com>
2026-04-19 09:01:02 +0100

    Cretated models and modelling

---
+++ Models/Enrollment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Enrollment
+{
+	public Guid ID { get; set; }
+
+	public Guid StudentID { get; set; }
+	public Student Student { get; set; } = null!;
+
+	public Guid CourseID { get; set; }
+	public Course Course { get; set; } = null!;
+
+	public EnrollmentStatus Status { get; set; }
+	public DateTime EnrolledAt { get; set; } = DateTime.UtcNow;
+
+}
---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Student
+{
+	public Guid ID { get; set; }
+	public string FullName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string UserID { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	public bool IsDelete { get; set; } = true;
+
+	public IColletion<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+}
